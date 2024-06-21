4e79aff7a2fb3da29fdfb3e1ebb58d445c0e635b
kaow-dev-2024 <kaow.dev.2024@gmail.com>
2024-06-21 17:13:37 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app>
+    <v-card flat>
+      <v-card-title>
+        <v-bottom-navigation background-color="dark" dark shift grow>
+          <v-btn
+            v-for="(item, i) in ItemsMenu"
+            :key="i"
+            :to="item.link"
+            @click="setTitle(item)"
+          >
+            <span>
+              {{ item.text }}
+            </span>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+      </v-card-title>
+    </v-card>
+
+    <v-divider></v-divider>
+
+    <v-card flat>
+      <v-card-title>
+        <v-alert text prominent type="warning" :icon="icon">
+          <div class="title">
+            {{ title }}
+          </div>
+        </v-alert>
+      </v-card-title>
+    </v-card>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      title: 'HOME',
+      icon: 'mdi-home',
+      ItemsMenu: [
+        {
+          text: 'HOME',
+          icon: 'mdi-home',
+          link: '/',
+        },
+        {
+          text: 'CONTACT PERSON',
+          icon: 'mdi-badge-account',
+          link: '/contact-person',
+        },
+        {
+          text: 'EDUCATION',
+          icon: 'mdi-school-outline',
+          link: '/education',
+        },
+        {
+          text: 'WORK EXPERIENCE',
+          icon: 'mdi-briefcase-outline',
+          link: '/work-experience',
+        },
+        {
+          text: 'SKILL',
+          icon: 'mdi-basket-fill',
+          link: '/skill',
+        },
+      ],
+    }
+  },
+
+  methods: {
+    setTitle(item) {
+      this.title = item.text
+      this.icon = item.icon
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-container>
+      <v-card>
+        <v-card-actions>
+          <!-- <v-img src="" /> -->
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
